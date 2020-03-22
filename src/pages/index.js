aa656efeda36bb53ca2ba8d23104dcf0d8378051
Viren Bhagat <virenb@gmail.com>
2020-03-22 14:41:13 -0400

    Small margin update

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,9 @@ const IndexPage = ({ data }) => {
       <SEO title="Home" />
       <h2>Hej World</h2>
       <div>
-        <p>
+        <p style={{
+          marginBottom: `1rem`
+        }}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
           incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
           exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute

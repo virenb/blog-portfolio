e1498c4924ac7df929c97cbf7f4122e8da42bccc
virenb <virenb@gmail.com>
2020-08-25 16:45:27 -0400

    Changed gatsby-node to create mdx pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,34 +1,69 @@
-const path = require(`path`)
+const path = require("path")
+// createPages for Contentful CMS
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   const blogPostTemplate = path.resolve(`src/templates/blogPost.js`)
+//   const result = await graphql(`
+//   {
+//     allContentfulBlogPost {
+//       edges {
+//         node {
+//           slug
+//           title
+//           id
+//         }
+//       }
+//     }
+//   }
+//   `)
 
-exports.createPages = async ({ graphql, actions }) => {
+//   if (result.errors) {
+//     console.log(result.errors)
+//   }
+
+//   result.data.allContentfulBlogPost.edges.forEach(edge => {
+//     createPage({
+//       path: `${edge.node.slug}`,
+//       component: blogPostTemplate,
+//       context: {
+//         slug: edge.node.slug,
+//         title: edge.node.title
+//       }
+//     })
+//   })
+// }
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const template = path.resolve(`./src/templates/mdx-posts-layout.js`)
   const { createPage } = actions
-  const blogPostTemplate = path.resolve(`src/templates/blogPost.js`)
   const result = await graphql(`
-  {
-    allContentfulBlogPost {
-      edges {
-        node {
-          slug
-          title
-          id
+    query {
+      allMdx {
+        edges {
+          node {
+            id
+            slug
+          }
         }
       }
     }
-  }
   `)
-
   if (result.errors) {
-    console.log(result.errors)
+    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query')
   }
-
-  result.data.allContentfulBlogPost.edges.forEach(edge => {
+  // Create blog post pages.
+  const posts = result.data.allMdx.edges
+  // you'll call `createPage` for each result
+  posts.forEach(({ node }, index) => {
     createPage({
-      path: `${edge.node.slug}`,
-      component: blogPostTemplate,
-      context: {
-        slug: edge.node.slug,
-        title: edge.node.title
-      }
+      // This is the slug you created before
+      // (or `node.frontmatter.slug`)
+      path: `${node.slug}`,
+      // This component will wrap our MDX content
+      component: template,
+      // You can use the values in this context in
+      // our page layout component
+      context: { id: node.id },
     })
   })
 }

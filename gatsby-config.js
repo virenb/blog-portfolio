fe36bbb7792828024971a41000061f214cf9cfbe
virenb <virenb@gmail.com>
2020-08-25 16:38:50 -0400

    Removing contentful, adding mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     author: `@virengb`,
   },
   plugins: [
+    `gatsby-plugin-mdx`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     {
@@ -16,6 +17,13 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: `${__dirname}/src/posts/`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -31,55 +39,19 @@ module.exports = {
         theme_color: `#2e8b57`,
         display: `minimal-ui`,
         icon: `src/images/apple-touch-icon.png`,
-        // icons: [
-        //   {
-        //     src: `src/images/android-chrome-192x192.png`,
-        //     sizes: `192x192`,
-        //     type: `image/png`,
-        //   },
-        //   {
-        //     src: `src/images/android-chrome-512x512.png`,
-        //     sizes: `512x512`,
-        //     type: `image/png`,
-        //   },
-        //   {
-        //     src: `src/images/apple-touch-icon.png`,
-        //     sizes: `180x180`,
-        //     type: `image/png`,
-        //   },
-        //   {
-        //     src: `src/images/favicon-16x16.png`,
-        //     sizes: `16x16`,
-        //     type: `image/png`,
-        //   },
-        //   {
-        //     src: `src/images/favicon-32x32.png`,
-        //     sizes: `32x32`,
-        //     type: `image/png`,
-        //   },
-        //   {
-        //     src: `src/images/favicon.ico`,
-        //     sizes: `48x48`,
-        //     type: `image/ico`,
-        //   },
-        // ]
-      },
     },
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: `yzwpq1epaq68`,
-        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-contentful`,
+    //   options: {
+    //     spaceId: `yzwpq1epaq68`,
+    //     accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-canonical-urls`,
       options: {
-        siteUrl: `https://virenb.cc`
-      }
-    }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+        siteUrl: `https://virenb.cc`,
+      },
+    },
   ],
 }

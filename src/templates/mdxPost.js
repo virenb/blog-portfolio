56927967f058b6ed0372874d4ab3514964666cd1
virenb <virenb@gmail.com>
2020-09-12 17:28:49 -0400

    Removed tags from template

--- src/templates/mdxPost.js
+++ src/templates/mdxPost.js
@@ -11,12 +11,7 @@ export default function PostTemplate({ data: { mdx } }) {
     <PostLayout>
       <h1>{mdx.frontmatter.title}</h1>
       {new Date(mdx.frontmatter.date).toLocaleDateString()}
-      <div>
-        {mdx.frontmatter.tags
-          .map(idx => '#' + idx)
-          .join(', ')
-          .split('')}
-      </div>
+      <div>{mdx.frontmatter.tags.join(', ').split('')}</div>
       <br />
 
       <MDXRenderer>{mdx.body}</MDXRenderer>
